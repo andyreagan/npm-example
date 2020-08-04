4422fb5cd86d00f880c09e8238c92a89318d419d
Andy Reagan <areagan@massmutual.com>
2020-08-04 15:45:49 -0400

    Declare rollup dev dep in package.json, add function that depends on d3 global, bump to 1.1.1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+import * as d3 from 'd3'
+
 export var printMsg = function() {
   console.log("This is a message from the demo package");
+}
+
+export var functionThatDependsOnD3 = function() {
+  console.log(d3.version);
 }
